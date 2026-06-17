1c272d9988bbed1e24498f7c846e9e0573649725
sararangel04 <1221424@isep.ipp.pt>
2026-06-17 18:29:34 +0100

    chore: remover Fluxo Clínico do sidebar do administrador

--- includes/sidebar_admin.php
+++ includes/sidebar_admin.php
@@ -44,10 +44,6 @@ $pa = $pagina_ativa ?? '';
                class="nav-link<?= menuAdmin('atribuicoes', $pa) ?>" style="padding-left:2.2rem;font-size:.88rem;">
                 <i class="fa-solid fa-arrows-left-right-to-line me-2"></i>Atribuições
             </a>
-            <a href="<?= APP_URL ?>/private/admin/fluxo_clinico/confirmar_tarefas.php"
-               class="nav-link<?= menuAdmin('fluxo_clinico', $pa) ?>">
-                <i class="fa-solid fa-clipboard-list me-2"></i>Fluxo Clínico
-            </a>
             <a href="<?= APP_URL ?>/private/admin/seguranca/logs_acesso.php"
                class="nav-link<?= menuAdmin('seguranca', $pa) ?>">
                 <i class="fa-solid fa-shield me-2"></i>Segurança
